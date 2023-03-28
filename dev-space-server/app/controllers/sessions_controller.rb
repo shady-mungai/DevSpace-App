8414fdf73cebe2455277a1d97582457086ae33c6
shady-mungai <shadrackmungai10@gmail.com>
2023-03-29 00:15:28 +0300

    Initialized sessions contoller and added necessary routes to login a user and logout using sessions

---
+++ dev-space-server/app/controllers/sessions_controller.rb
@@ -0,0 +1,24 @@
+class SessionsController < ApplicationController
+
+    #POST to enable a user to login via finding user by email 
+    #authentucating the user's password
+    #storing their id via sessions
+    def create
+        
+        user = User.find_by(email: params[:email])
+        if user&.authenticate(params[:password])
+            session[:user_id] = user.id
+            render json: user, status: :ok
+        else
+            render json: { error: "User not found" }, status: :not_found
+        end
+   end
+
+   #delete functionality that deletes the user's id from the sessions
+   def destroy
+       session.delete :user_id
+       head :no_content
+       
+   end
+
+end
