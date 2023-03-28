0d58d2317a414554f085a9563f3666de8633f0e7
shady-mungai <shadrackmungai10@gmail.com>
2023-03-29 00:16:13 +0300

    Added necessary comments

--- dev-space-server/app/models/user.rb
+++ dev-space-server/app/models/user.rb
@@ -1,10 +1,12 @@
 class User < ApplicationRecord
-    has_secure_password
     #validations
-    validates :password_digest, presence: true
+    validates :password, presence: true
     validates :username, uniqueness: true,presence: true
     validates :email, presence: true, uniqueness: true
     
+    # helper method to encrypt password
+    has_secure_password
+
     # relationships
     has_many :articles
     has_many :reviews
