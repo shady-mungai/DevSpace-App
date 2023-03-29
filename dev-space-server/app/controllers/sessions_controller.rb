4c0c51a9b7f4c674ac61c4148c8ee069ec400482
shady-mungai <shadrackmungai10@gmail.com>
2023-03-29 08:04:02 +0300

    Changed name of route methods

--- dev-space-server/app/controllers/sessions_controller.rb
+++ dev-space-server/app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@ class SessionsController < ApplicationController
     #POST to enable a user to login via finding user by email 
     #authentucating the user's password
     #storing their id via sessions
-    def create
+    def login
         
         user = User.find_by(email: params[:email])
         if user&.authenticate(params[:password])
@@ -14,11 +14,10 @@ class SessionsController < ApplicationController
         end
    end
 
-   #delete functionality that deletes the user's id from the sessions
-   def destroy
+   #delete functionality that deletes the user's id from the sessions thus logging out user
+   def logout 
        session.delete :user_id
        head :no_content
-       
-   end
+    end
 
 end
