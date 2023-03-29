2551a8db6870ee402088d9ba66819a64014e36e6
shady-mungai <shadrackmungai10@gmail.com>
2023-03-29 22:22:48 +0300

    Initialized create method for the reviews

--- dev-space-server/app/controllers/reviews_controller.rb
+++ dev-space-server/app/controllers/reviews_controller.rb
@@ -1,2 +1,13 @@
 class ReviewsController < ApplicationController
+    before_action :authorize
+
+    def create 
+        review = user.articles.reviews.create(review_params)
+    end
+    
+
+    private 
+    def review_params
+        params.permit(:content,:likes)
+    end
 end
