0c589b0e9efd5ae6235429c5f75154cb2f5889b6
shady-mungai <shadrackmungai10@gmail.com>
2023-03-30 23:48:14 +0300

    Solved ana error in the controller

--- dev-space-server/app/controllers/application_controller.rb
+++ dev-space-server/app/controllers/application_controller.rb
@@ -8,8 +8,6 @@ class ApplicationController < ActionController::API
         session[:uid] = id
     end
     
-    end   
-    
     def authorize
         render json: { error: "Not authorized" }, status: 401 unless session.include? :user_id
     end
