d6a48b141c943043fe262cbdc0d9eb741ddef6a7
ryan605 <langatryan26@gmail.com>
2023-03-30 15:50:18 +0300

    done

--- dev-space-server/app/controllers/articles_controller.rb
+++ dev-space-server/app/controllers/articles_controller.rb
@@ -1,7 +1,53 @@
-class ArticlesController < ApplicationController
-    #GET all articles
+<<<<<<< HEAD
+class ArticlesController < ApplicationController 
+
+  before_action :authorize
+
+  #GET all articles
+
     def index 
-        articles = Article.all
-        render json: articles
+      articles = Article.all
+      render json: articles
+    end
+    
+
+    # POST create a new article
+    def create
+      article = user.articles.create!(article_params)
+      if article
+        render json: article,status: :created
+      else
+        render json: {error: article.errors.full_messages}, status: :unprocessable_entity
+      end
+    end
+
+    #PUT update an article
+
+    def update
+      article = Article.find_by(id:params[:id])
+      if article
+        article.update(article_params)
+        render json: article,status: :accepted
+      else
+        render json: {error: "Article not found"}, status: :not_found
+      end
+    end
+
+    #destroy
+    def destroy
+      article = Article.find_by(id:params[:id])
+      if article
+        article.reviews.destroy_all # delete all associated reviews
+        article.destroy
+        render json: []
+      else
+        render json: {error: "Article not found"}, status: :not_found
+      end
+    end
+  
+    private
+    def article_params
+      params.permit(:title,:topic_language,:content)
     end
 end
+=======
