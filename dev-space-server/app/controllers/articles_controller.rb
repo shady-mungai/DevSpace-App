92d96199342ab1bc5c60bb0a2d195cd335c16728
mbugua <mbuguafaimba@gmail.com>
2023-03-30 10:27:47 +0300

    fetched server

--- dev-space-server/app/controllers/articles_controller.rb
+++ dev-space-server/app/controllers/articles_controller.rb
@@ -1,7 +1,14 @@
 class ArticlesController < ApplicationController
+    before_action :authorize
+
     #GET all articles
+
     def index 
         articles = Article.all
         render json: articles
     end
+
+    def create 
+
+
 end
--- dev-space-server/app/models/user.rb
+++ dev-space-server/app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
     #validations
-    # validates :password, presence: true
-    # validates :username, uniqueness: true,presence: true
-    # validates :email, presence: true, uniqueness: true
+    validates :password, presence: true
+    validates :username, uniqueness: true,presence: true
+    validates :email, presence: true, uniqueness: true
     
     # helper method to encrypt password
     has_secure_password
