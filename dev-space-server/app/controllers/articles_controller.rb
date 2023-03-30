7352c956436b90b33abf11a15c723cf51090b78f
ryan605 <langatryan26@gmail.com>
2023-03-30 12:52:20 +0300

    update article

--- dev-space-server/app/controllers/articles_controller.rb
+++ dev-space-server/app/controllers/articles_controller.rb
@@ -14,9 +14,20 @@ class ArticlesController < ApplicationController
         render json: {error: article.errors.full_messages}, status: :unprocessable_entity
       end
     end
+
+    #PUT update an article
+
+    def update
+      article = Article.find_by(id:params[:id])
+      if article
+        article.update(article_params)
+        render json: article,status: :accepted
+      else
+        render json: {error: "Article not found"}, status: :unprocessable_entity
+      end
+    end
   
     private
-  
     def article_params
       params.permit(:title,:topic_language,:content)
     end
--- dev-space-server/config/routes.rb
+++ dev-space-server/config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   post "/login", to: "sessions#login"
   # resources :tags
   # resources :reviews
-   resources :articles, only:[:index, :create]
+   resources :articles, only:[:index, :create,:update]
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
