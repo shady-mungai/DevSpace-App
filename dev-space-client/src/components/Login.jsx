1e5a8db8d8cf631f31ca3357c1050a7099077831
Fleur41 <Muriithikings456@gmail.com>
2023-04-03 00:19:22 +0300

    login.jsx

--- dev-space-client/src/components/Login.jsx
+++ dev-space-client/src/components/Login.jsx
@@ -1,82 +1,93 @@
-import React from "react";
-import './Login.css'
-import { Link } from "react-router-dom";
-import { useRef } from "react";
-import { useNavigate } from "react-router-dom";
-
-
-
+import React, { useRef, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import './Login.css';
 
 const Login = () => {
-    const navigate = useNavigate()
+  const [users, setUsers] = useState([])
+  const navigate = useNavigate();
+  const emailRef = useRef();
+  const passwordRef = useRef();
+  
 
-//  const {currentUser , userLogin,signInWithGoogle,forgot} = useContext(AuthContext)
+  const userLogin = async () => {
+    const response = await fetch('http://localhost:3300/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        email: emailRef.current.value,
+        password: passwordRef.current.value
+      })
+    });
 
-const emailRef = useRef()
-const passwordRef = useRef()
+    if (response.ok) {
+          fetch('http://localhost:3300/users')
+          .then(response => response.json())
+          .then(data => {
+            console.log(data);
+            let user = data.filter(
+              (user)=> user.email=== emailRef.current.value
+            )
+            setUsers(user);
+            console.log(user)
+            
+          })
+          .catch(error => {
+            console.error('Error:', error);
+          });
 
-const userLogin = () =>{
-    fetch("http://localhost:3000/login",{
-        method: "POST",
-        headers: {
-            "content-type" : "application/json"
-        },
-    body : JSON.stringify()
-    })
-}
-// const forgotPasswordHandler = ()=>{
-//     const email = emailRef.current.value
-//     if(email) forgot(email).then(()=>{
-//         emailRef.current.value=""
-//         alert('Password reset email has been sent')
-//     })
-// }
 
-    return ( 
-        
-       <> 
-        <div className="loginPage">
-            <div className="containerone">
-            <div className="form">
-                <h1>LOGIN </h1>
-                <div className="lines">
-                    <div className="lineone"></div>
-                    <p>Sign in with email</p>
-                    <div className="line"></div>
-                </div>
-               
-                <form onSubmit={(e)=>{
-                    e.preventDefault()
-                        userLogin()
-                        navigate("/")
-                    }}>
-                   <div className="div"><p>Email*</p></div> 
-                    <div className="emailplace"><input ref={emailRef} type="email" placeholder="johndoe@gmail.com" required /></div>
-                
-                
-                    <div className="div"><p>Password*</p></div>
-                    <div className="passplace"><input ref={passwordRef} type="password" placeholder="Input password" required/></div>
-                
-                <div className="forgot">
-                    <p>Forgot password?</p>
-                </div>
-                <div className="loginBtn">
-                    <input type='submit'value='login'/>
-                </div>
-                </form>
-                <div className="notregisterd">
-                    <p>Not registered yet?</p>
-                   <Link to='/SignUp'>Create an Account</Link>
-                </div>
+    // filer 
+
+
+       navigate('/Articles');
+    } else {
+      // Login failed, display error message
+      const error = await response.json();
+      alert(error.message);
+    }
+  };
+
+  return (
+    <>
+      <div className="loginPage">
+        <div className="containerone">
+          <div className="form">
+            <h1>LOGIN </h1>
+            <div className="lines">
+              <div className="lineone"></div>
+              <p>Sign in with email</p>
+              <div className="line"></div>
             </div>
+            <form onSubmit={(e) => {
+              e.preventDefault();
+              userLogin();
+            }}>
+              <div className="div"><p>Email*</p></div>
+              <div className="emailplace"><input ref={emailRef} type="email" placeholder="johndoe@gmail.com" required /></div>
+              <div className="div"><p>Password*</p></div>
+              {users.map((user)=>(
+                <div className="div"><p>{user.email}</p></div>
+              ))}
+              <div className="passplace"><input ref={passwordRef} type="password" placeholder="Input password" required /></div>
+              <div className="forgot">
+                <p>Forgot password?</p>
+              </div>
+              <div className="loginBtn">
+                <input type='submit' value='login' />
+              </div>
+            </form>
+            <div className="notregisterd">
+              <p>Not registered yet?</p>
+              <Link to='/SignUp'>Create an Account</Link>
             </div>
+          </div>
         </div>
+      </div>
+      
+    </>
+  );
+};
 
-        
-
-       </>
-
-     );
-}
- 
 export default Login;
