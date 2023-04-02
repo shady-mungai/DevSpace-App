fb9a0ea95ecd37ffeddf2ce44b053e42a13009a8
mbugua <mbuguafaimba@gmail.com>
2023-04-02 08:19:28 +0300

    added some more functionality

--- dev-space-client/src/components/Articles.js
+++ dev-space-client/src/components/Articles.js
@@ -1,112 +1,95 @@
-import './Articles.css';
-import React from 'react';
+import "./Articles.css";
+import React, { useState, useEffect, useContext } from 'react';
+import {  Link } from 'react-router-dom';
+import AuthContext from "./AuthContext/AuthContext";
+import Profile from "./Profile";
+
 
 const Articles = () => {
-    return (
-        <>
-             <header>
+
+  const {currentUser} = useContext(AuthContext)
+  const [articles, setArticles] = useState([]);
+  const [searchQuery, setSearchQuery] = useState('');
+
+  useEffect(() => {
+    const fetchArticles = async () => {
+      const response = await fetch('http://localhost:3300/articles?_embed=tags');
+      if (response.ok) {
+        const data = await response.json();
+        setArticles(data);
+        console.log(data)
+      } else {
+        console.error('Failed to fetch articles:', response.status);
+      }
+    };
+    fetchArticles();
+  }, []);
+  const filteredArticles = articles.filter((article) => {
+    const titleMatch = article.title.toLowerCase().includes(searchQuery.toLowerCase());
+    const tagMatch = article.tags.some((tag) => tag.name.toLowerCase().includes(searchQuery.toLowerCase()));
+    const contentMatch = article.content && JSON.parse(article.content).some((content) => content.toLowerCase().includes(searchQuery.toLowerCase()));
+    return titleMatch || tagMatch || contentMatch ;
+  });
+
+ useEffect(()=>{
+    console.log(currentUser)
+ },[currentUser])
+
+  return (
+    <div className="Homepage">
+      <header>
         <div className="navbar">
-              <div className="left">
-                    <h1>DevSpace</h1>
-                    <input type="search" placeholder="Search"/>
-              </div>
-              <div className="links">
-                    <img src="" alt="profile" />
-               </div>
+          <div className="left">
+            <h1>DevSpace {currentUser && currentUser.id}</h1>
+            <input type="search" placeholder="Search" value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)} />
+
+          </div>
+          <div className="links">
+
+            <Profile />
+          </div>
         </div>
       </header>
-
-
-      <div className="body">
-
+      <div className="main-body">
         <div className="tags">
-          <a href="#">Front-End</a> 
+          <a href="#">Front-End</a>
           <a href="#">Coding</a>
           <a href="#">Software development</a>
-          <a href="#">Back-end</a>  
-          <a href="#">Fullstack</a>   
-          <a href="#">Javascript</a>  
+          <a href="#">Back-end</a>
+          <a href="#">Fullstack</a>
+          <a href="#">Javascript</a>
         </div>
+        
         <div className="articles">
-            <div className="article">
+          {filteredArticles.map((article) => (
+            <div className="article" key={article.id}>
               <div className="article-profile">
                 <img src="" alt="icon" />
-                <h3 className='name'>Morty Smith</h3>
+                <h3 className="name">{article.user.username}</h3>
+                
               </div>
               <div className="article-content">
-              <h1 className='title'>Can <a href="#">ChatGPT Write Better SQL than a Data Analyst?</a></h1>
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque, iste alias deserunt nisi at nulla nobis laboriosam, provident autem laudantium eum optio expedita iusto libero sit saepe iure et hic!
-              </p>
+                <h1 className="title">
+                  <Link to={`/articles/${article.id}`}>{article.title}</Link>
+                </h1>
+                <p className="content">
+                {article.content && JSON.parse(article.content)}
+                  </p>
               </div>
               <footer>
-                <p className="tag">Fullstack</p>
+                <div className="tags-container">
+                  {article.tags.map((tag) => (
+                    <p className="tag" key={tag.id}>{tag.name}</p>
+                  ))}
+                </div>
                 <p>6 min read</p>
               </footer>
             </div>
+          ))}
         </div>
-
-        <div className="articles">
-            <div className="article">
-              <div className="article-profile">
-                <img src="" alt="icon" />
-                <h3 className='name'>Morty Smith</h3>
-              </div>
-              <div className="article-content">
-              <h1 className='title'>Can <a href="#">ChatGPT Write Better SQL than a Data Analyst?</a></h1>
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque, iste alias deserunt nisi at nulla nobis laboriosam, provident autem laudantium eum optio expedita iusto libero sit saepe iure et hic!
-              </p>
-              </div>
-              <footer>
-                <p className="tag">Fullstack</p>
-                <p>6 min read</p>
-              </footer>
-            </div>
-        </div>
-
-        <div className="articles">
-            <div className="article">
-              <div className="article-profile">
-                <img src="" alt="icon" />
-                <h3 className='name'>Morty Smith</h3>
-              </div>
-              <div className="article-content">
-              <h1 className='title'>Can <a href="#">ChatGPT Write Better SQL than a Data Analyst?</a></h1>
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque, iste alias deserunt nisi at nulla nobis laboriosam, provident autem laudantium eum optio expedita iusto libero sit saepe iure et hic!
-              </p>
-              </div>
-              <footer>
-                <p className="tag">Fullstack</p>
-                <p>6 min read</p>
-              </footer>
-            </div>
-        </div>
-
-        <div className="articles">
-            <div className="article">
-              <div className="article-profile">
-                <img src="" alt="icon" />
-                <h3 className='name'>Morty Smith</h3>
-              </div>
-              <div className="article-content">
-              <h1 className='title'>Can <a href="#">ChatGPT Write Better SQL than a Data Analyst?</a></h1>
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque, iste alias deserunt nisi at nulla nobis laboriosam, provident autem laudantium eum optio expedita iusto libero sit saepe iure et hic!
-              </p>
-              </div>
-              <footer>
-                <p className="tag">Fullstack</p>
-                <p>6 min read</p>
-              </footer>
-            </div>
-        </div>
-
       </div>
+    </div>
+  );
+};
 
-        </>
-    );
-}
- 
 export default Articles;
