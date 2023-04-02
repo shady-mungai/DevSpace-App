3ea6c40d752112bfdb1b4429d19e4f31d76a8c82
Fleur41 <Muriithikings456@gmail.com>
2023-04-03 00:15:06 +0300

    app.js

--- dev-space-client/src/App.js
+++ dev-space-client/src/App.js
@@ -1,12 +1,15 @@
-import React from "react"
-import { Routes, Route } from "react-router-dom"
+import React from "react";
+import { Routes, Route } from "react-router-dom";
 import Login from "./components/Login";
 import SignUp from "./components/SignUp";
-import HomePage from "./components/HomePage"
-
-
+import HomePage from "./components/HomePage";
 import './App.css';
-// import Articles from "./components/Articles";
+import Articles from "./components/Articles";
+import ArticlesId from "./components/ArticlesId";
+import Profile from "./components/Profile";
+import Library from "./components/Library";
+import ProfileOwner from "./components/ProfileOwner";
+import SignOut from "./components/SignOut";
 
 function App() {
   return (
@@ -15,7 +18,12 @@ function App() {
         <Route exact path="/" element={<HomePage />}/> 
         <Route path="/Login" element={<Login />}/>
         <Route path="/SignUp" element={<SignUp />}/>
-        {/* <Route path="/Articles" element={<Articles />} /> */}
+        <Route path="/Articles" element={<Articles />} /> 
+        <Route path="/articles/:id" element={<ArticlesId />} />
+        <Route path="/Profile" element={<Profile />} />
+        <Route path="/Library" element={<Library />} />
+        <Route path="/ProfileOwner" element={<ProfileOwner />} />
+        <Route path="/SignOut" element={<SignOut />} />
       </Routes>
     </div>
   );
