902f128fd94962c053bf833e34c2212b28e84960
mbugua <mbuguafaimba@gmail.com>
2023-04-02 08:27:58 +0300

    modified some functionality  in profileowner component

--- dev-space-client/src/components/ProfileOwner.js
+++ dev-space-client/src/components/ProfileOwner.js
@@ -1,33 +1,49 @@
-import React from "react";
+import React, { useContext } from "react";
+import { useEffect, useState } from "react";
+import AuthContext from "./AuthContext/AuthContext";
+const ProfileOwner = () => {
+const{currentUser,setCurrentUser} = useContext(AuthContext)
+// const [articles,setArticles] = useState([])
+  useEffect(() => {
+  fetch('http://localhost:3300/users')
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+    let user = data.filter(
+      (user)=> user.email=== currentUser && currentUser.email
+    )
+    setCurrentUser(user);
 
-const ProfileOwner = ({ email, username }) => {
-  const getInitials = (name) => {
-    if (!name) {
-      return "";
-    }
-    const parts = name.split(" ");
-    let initials = "";
-    for (let i = 0; i < parts.length; i++) {
-      initials += parts[i][0];
-    }
-    return initials.toUpperCase();
-  }
+  })
+  }, []);
+  
+  // const getInitials = (name) => {
+  //   if (!name) {
+  //     return "";
+  //   }
+  //   const parts = name.split(" ");
+  //   let initials = "";
+  //   for (let i = 0; i < parts.length; i++) {
+  //     initials += parts[i][0];
+  //   }
+  //   return initials.toUpperCase();
+  // }
 
-  const getProfilePicture = () => {
-    const initials = getInitials(username);
-    return <div className="profile-picture">{initials}</div>;
-  }
+  // const getProfilePicture = () => {
+  //   const initials = getInitials(username);
+  //   return <div className="profile-picture">{initials}</div>;
+  // }
 
   return (
-    <div className="card">
-      <div className="card-header">
-        <h3>Profile Owner</h3>
-      </div>
-      <div className="card-body">
-        {getProfilePicture()}
-        <p>Name: {username}</p>
-        <p>Email: {email}</p>
-      </div>
+    <div className="hero">
+        <div>
+               
+               <>
+                 <div className="div"><p>{currentUser && currentUser.username}</p></div>
+               <div className="div"><p>{currentUser && currentUser.email}</p></div>
+               </>
+     
+           </div>
     </div>
   );
 }
