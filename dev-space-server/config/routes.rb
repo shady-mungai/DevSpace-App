972a39aa7d324f87f895899fb1909ce6afc2a3db
Fleur41 <Muriithikings456@gmail.com>
2023-04-03 00:23:59 +0300

    updated routes

--- dev-space-server/config/routes.rb
+++ dev-space-server/config/routes.rb
@@ -1,21 +1,12 @@
 Rails.application.routes.draw do
   resources :users, only: [:index, :show]
  # post '/login',to: 'users#login'
-<<<<<<< HEAD
    post '/signup',to: 'users#signup'
    delete "/logout", to: "sessions#logout"
    post "/login", to: "sessions#login"
    resources :tags, only:[:index]
    resources :reviews, only: [:create]
-   resources :articles, only:[:index,:show,:destroy]
-=======
-  post '/signup',to: 'users#signup'
-  delete "/logout", to: "sessions#logout"
-  post "/login", to: "sessions#login"
-  # resources :tags
-  # resources :reviews
-   resources :articles, only:[:index]
->>>>>>> 71439411f8b8125aceaa49d65648e656f7179fc5
+   resources :articles, only:[:index,:show,:create,:update,:destroy]
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
