9a4a7f32aaf196021dca840b6295718d022ef5a4
mbugua <mbuguafaimba@gmail.com>
2023-04-02 08:27:08 +0300

    modified some functionality  in profile component

--- dev-space-client/src/components/Profile.js
+++ dev-space-client/src/components/Profile.js
@@ -1,16 +1,29 @@
-import React from "react";
-import ProfileOwner from "./ProfileOwner";
-import Library from "./Library";
-import SignOut from "./SignOut";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "./Profile.css";
 
 const Profile = () => {
+  const [showDropdown, setShowDropdown] = useState(false);
+
+  const toggleDropdown = () => {
+    setShowDropdown(!showDropdown);
+  };
+
   return (
     <>
-      <ProfileOwner />
-      <Library />
-      <SignOut />
+      <div className="profile" onClick={toggleDropdown}>
+        <p>Profile</p>
+        <i className="fa fa-caret-down"></i>
+      </div>
+      {showDropdown && (
+        <div className="dropdown">
+          <Link to="/ProfileOwner">Profile</Link>
+          <Link to="/Library">Library</Link>
+          <Link to="/SignOut">LogOut</Link>
+        </div>
+      )}
     </>
   );
-}
+};
 
 export default Profile;
